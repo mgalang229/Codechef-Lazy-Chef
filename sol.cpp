b5d95affdc57feff85c19fe20f84d8a8e3df6de4
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2021-05-30 20:39:30 +0800

    Create sol.cpp

---
+++ sol.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int tt;
+	cin >> tt;
+	while (tt--) {
+		int x, m, d;
+		cin >> x >> m >> d;
+    // find the minimum between the product of 'x' and 'm' and the sum of 'x' and 'd'
+		cout << min(x * m, x + d) << '\n';
+	}
+	return 0;
+}
